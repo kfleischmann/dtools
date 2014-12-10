213ab865eaa2b46528f6324bd039d0f807f077bc
kay <fleischmann.kay@googlemail.com>
2014-12-10 22:51:51 +0100

    dgen with parameters

--- dgen/src/dgen_ts.js
+++ dgen/src/dgen_ts.js
@@ -1,12 +1,15 @@
 var dgen = require("./dgen");
 
+var argv = process.argv;
+var count = argv[2];
 
 // Arithmetic sequence accepts start and step as a constructor parameters
 var period = new dgen.seqs.Arithmetic(0, Math.PI * 2 / 25)
 	.setMap(Math.sin);
 var noise = new dgen.seqs.Random();
 var trend = new dgen.seqs.Arithmetic(0, 0.05);
-var data = new dgen.seqs.Combine([period, noise, trend]).array(100)
+var data = new dgen.seqs.Combine([period, noise, trend]).array(count)
 
-
-console.log(data);
+data.forEach( function(value, i){
+  console.log(i+","+value);
+});
